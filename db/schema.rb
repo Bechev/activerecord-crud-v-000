c899f97c66a21865e117b92e8e975207b677af4c
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-15 15:53:57 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,4 @@
 
 ActiveRecord::Schema.define(version: 20180315144827) do
 
-  create_table "movies", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "release_date"
-    t.string   "director"
-    t.string   "lead"
-    t.boolean  "in_theaters"
-  end
-
 end
