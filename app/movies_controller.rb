7a60eed03854b3ce18e15e18896299f8e0404d96
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-15 14:53:28 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,7 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(args = {})
-  Movie.create do |m|
+  Movie.create :movies do |m|
     m.title :args[:title]
     m.release_date :args [:release_date]
     m.director :args [:director]
