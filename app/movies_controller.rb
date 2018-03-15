15555b4a408f7f7ff012c6aa1c82e0ce19559483
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-15 14:43:34 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,12 +24,12 @@ end
 
 def can_be_created_in_a_block(args = {__})
   Movie.create do |m|
-    __
+    # __
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  # __
 end
 
 def can_get_the_last_item_in_the_database
